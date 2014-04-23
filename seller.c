950738db4dd6dfd1112ef9badacb284d3ff317e7
f54849893 <gzliangjiahao@gmail.com>
2014-04-22 18:35:21 -0700

    copy from bidder.c, replace "bidder" with "seller"

--- seller.c
+++ seller.c
@@ -19,9 +19,7 @@
 
 #include "EE450.h"
 
-#define MAXDATASIZE 100 // max number of bytes we can get at once
-
-// get sockaddr, IPv4 or IPv6:
+// get sockaddr,
 void *get_in_addr(struct sockaddr *sa)
 {
 	if (sa->sa_family == AF_INET) {
@@ -32,7 +30,7 @@ void *get_in_addr(struct sockaddr *sa)
 }
 
 //read sellerPass.txt
-int readSellerPass(const char *filename, struct userNode **node){
+int readSellerPass(int sellerIndex, const char *filename, struct userNode **node){
 	FILE *fp = fopen(filename, "r");
 	char buffer[PASS_TXT_LINE_LEN] = {0};
 	char *p = buffer;
@@ -45,10 +43,11 @@ int readSellerPass(const char *filename, struct userNode **node){
 		p = buffer;
 
 		p = strtok(buffer, " ");
-		if ((node->type = atoi(p)) != 2){
+		if (((*node)->type = atoi(p)) != 2){
 			fprintf(stderr,"Not a seller: %d %s %s %s\n",(*node)->type,(*node)->name,(*node)->password,p);
 			return 1;
 		}
+		(*node)->userIndex = sellerIndex;
 		p = strtok(NULL, " ");
 		strcpy ((*node)->name,p);
 		p = strtok(NULL, " ");
@@ -57,9 +56,10 @@ int readSellerPass(const char *filename, struct userNode **node){
 		if (strlen(p) == 9 && strncmp(p,"4519",4) == 0)
 			strcpy ((*node)->accountNum,p);
 		else{
-			fprintf(stderr,"%s %s %s %s\n","Wrong Bank Account:",(*node)->name,(*node)->password,p);
+			fprintf(stderr,"Wrong Bank Account: %s %s %s\n",(*node)->name,(*node)->password,p);
 			return 1;
 		}
+
 	}
 	else{
 		fprintf(stderr,"Can't read any information from %s\n",filename);
@@ -76,17 +76,39 @@ int main(void)
 	char buf[MAXDATASIZE];
 	struct addrinfo hints, *servinfo, *p;
 	int rv;
-	char s[INET6_ADDRSTRLEN];
+	char s[INET_ADDRSTRLEN];
+	struct sockaddr_in sa;	//store local address
+	int sa_len = sizeof(sa);
 
+	int cpid;
+
+	struct userNode* sellerInfo;
+	sellerInfo = malloc(sizeof(struct userNode));
+	memset(sellerInfo,0,sizeof(struct userNode));
 
 	/*phase 1: Authorization*/
 	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
+	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
 
 	if ((rv = getaddrinfo(HOSTNAME, PORT_S_P1, &hints, &servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
+	}
+
+	cpid = fork();
+	if(cpid){
+		//parent process
+		if (readSellerPass(2, "sellerPass2.txt", &sellerInfo) != 0){	//read sellerpass1.txt and load user information
+				perror("sellerPass1.txt");
+				return 1;
+			}
+		sleep(2);	//child sleep 2s, wait until parent finished
+	}else{
+		//child process
+		if (readSellerPass(1, "sellerPass1.txt", &sellerInfo) != 0){	//read sellerpass1.txt and load user information
+						perror("sellerPass1.txt");
+						return 1;}
 	}
 
 	// loop through all the results and connect to the first we can
@@ -111,22 +133,49 @@ int main(void)
 		return 2;
 	}
 
+	//Phase 1: <Seller#>__ has TCP port ___ and IP address: ____
+	getsockname(sockfd, (struct sockaddr*)&sa, &sa_len);
+	inet_ntop(AF_INET, (void*)&(sa.sin_addr), s, sizeof s);
+
+	if(cpid){
+		//parent process
+		printf("Phase 1: <Seller2> has TCP port %d and IP address: %s\n",	sa.sin_port, s);
+	}else{
+		//child process
+		printf("Phase 1: <Seller1> has TCP port %d and IP address: %s\n",	sa.sin_port, s);
+	}
+
+#ifdef DEBUG
 	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
 			s, sizeof s);
-	printf("client: connecting to %s\n", s);
-
+	printf("client: connecting to %s Port:%d\n", s, ((struct sockaddr_in*)(p->ai_addr))->sin_port);
+#endif
 	freeaddrinfo(servinfo); // all done with this structure
 
+	//Login cammand: "Login#type userIndex username password bankaccount"
+	sprintf(buf,"Login#%d %d %s %s %s",sellerInfo->type, sellerInfo->userIndex,
+			sellerInfo->name, sellerInfo->password, sellerInfo->accountNum);
+	printf("Phase 1: Login request. User:%s password:%s Bank account:%s\n",
+			sellerInfo->name, sellerInfo->password, sellerInfo->accountNum);
+#ifdef DEBUG
+	puts(buf);
+#endif
+	if ((numbytes = send(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+		    perror("recv");
+		    exit(1);
+		}
+	sleep(1); //sleep 1 second
 	if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
 	    perror("recv");
 	    exit(1);
 	}
-
 	buf[numbytes] = '\0';
-
-	printf("client: received '%s'\n",buf);
-
+	printf("Phase 1: Login request reply: %s .\n", buf);
+//	printf("client: received '%s'\n",buf);
 	close(sockfd);
+	/*End of phase 1*/
+
+
 
 	return 0;
 }
